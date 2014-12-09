1e96a41227b51b8bfe66177ec90895eea7711396
David Schenkel <david.schenkel@gmx.net>
2014-12-09 14:18:29 +0100

    presentation draft working

--- R/LAIre_bimonthly.R
+++ R/LAIre_bimonthly.R
@@ -1,23 +1,28 @@
 library(sp)
 library(raster)
-library(ncdf)
 library(fields)
 library(rgdal)
+library(ncdf)
 
 month <- c("jana","janb","feba","febc","mara","marb","apra","aprb","maya","mayb","juna","junb","jula","julb","auga","augb","sepa","sepb","octa","octb","nova","novb","deca","decb")
 
-t <- 1:365
+
+
+file.name = paste("~/Documents/Uni/Masterarbeit/LAIre/Global-0.5x0.5.analysis.1985.nc", sep="") 
+file <- open.ncdf(file.name)
+file.lai <- get.var.ncdf(file, "LAI")
+is.array(file.lai)
+t <- length(get.var.ncdf(file,"time"))
+
 if(length(t) < 366){
 	month.ndays <- c(15,16,14,14,15,16,15,15,15,16,15,15,15,16,15,16,15,15,15,16,15,15,15,16)
 	} else {
 	month.ndays <- c(15,16,14,15,15,16,15,15,15,16,15,15,15,16,15,16,15,15,15,16,15,15,15,16)
 }
-length(month.ndays)
 	
 y <- factor(rep(month, month.ndays))
-y
 
-tapply()...
+aggregate(file.lai,by = as.list(y), FUN = sum )
 
 for (year in 1982:2011) {
 	for (i in 1:24) {
